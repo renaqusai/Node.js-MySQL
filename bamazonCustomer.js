2c4a90db9ce978d9dd7db8b934b680b4b7e2ba59
Requ Amer <requamer@Requs-MacBook-Pro.local>
2018-12-09 16:07:57 -0800

    edition

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,108 +1,129 @@
-var mysql = require ('mysql');
-var inquirer = require('inquirer');
-// allows for color in the console
-var colors = require('colors');
+var mysql = require("mysql");;
+var inquirer = require("inquirer");
+var table = require("console.table");
+
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'password',
-    database: 'bamazon',
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazonDB"
 });
 
-// then create a call back function to confirm if connection was successful:
-connection.connect(function(err){
-    if(err){
-        throw err;
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("\nWelcome to BAmazon! Take a look at our products for sale below!\n");
+   
+    allProducts();
+});
+
+function allProducts() {
+    // query the database for all items for sale
+    connection.query("SELECT * from products;", function(err, results, fields) {
+        if (err) throw err;
+        else {
+        // console log all products
+        console.table(results);
+        
+      }
+      pickProduct();
+     
     }
-    console.log ("connected as id" + connection.threadid);  //threadid is a process id from mySQL, does not need to be defined here
+    
+)}
 
-    connection.query("SELECT * FROM products", function(err,res){
-        if(err) throw err;
-        for (var i=0; i<res.length; i++) {
-        console.log("Available for sale - ".green + (colors.yellow( "Item ID: " + res[i].item_id + "," + " Product: " 
-        + res[i].product_name + "," + " $" + res[i].price)));
-        //console.log(res[0].item_id);
-        }
-        start(res);
-    });
-});
-
-function start (results) { 
-
-  
-    inquirer.prompt([
+function pickProduct() {
+    inquirer
+        .prompt([
         {
-            // type is limited to "input" or other specific commands (input, confirm, list, rawlist, etc..).  Name field is freeform.
-            type: "list",
-            name: "selectItemID",
-            message: "What is the Item ID of the product you would like to buy?",
-            choices: function() {
-                var choiceArrayItemId = [];
-                for (var i = 0; i < results.length; i++) {
-                  choiceArrayItemId.push(results[i].item_id.toString());
-                }
-                return choiceArrayItemId;
-            }
+          name: "product",
+          type: "input",
+          message: "What is the item_id of the product you would like to buy?"
         },
         {
-            type: "input",
-            name: "selectNumUnits",
-            message: "How many would you like to buy?"
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to buy?"
         }
+        ])
+        .then(function(answer) {
+            
+            
+            var product = answer.product;
+            var quantity = answer.quantity;
+            
+            var queryProducts = "SELECT * FROM products WHERE ?";
+            var cost 
+            connection.query(queryProducts, {item_id: product}, function(err, res) {
+                var productInfo = res[0];
+                if (err) throw err;
+                if (quantity > productInfo.stock_quantity) {
+                    console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    console.log("I'm sorry we don't have enough in stock, choose a smaller quantity!");
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                    allProducts()
+                    
+                }
+                
+                 else {
+                   
+                    if (quantity <= productInfo.stock_quantity) {
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        console.log("We have " + quantity + " " + productInfo.product_name + "s in stock for your order!")
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        console.log("Thank you for your order! Please wait while we process your order!");
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                    } 
+                    if (cost = quantity * productInfo.price) {
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        console.log("The total cost of your order is $" + cost + ".00");
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                    }
+                    
+            var queryUpdate = "UPDATE products SET ? WHERE ?"
+            connection.query(queryUpdate, [{stock_quantity: answer.quantity},{item_id: product}], function(err, res) {
+                 if (err) throw err;
+                 else  {   
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");  
+                    console.log("Inventory has been updated!");
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+                                   
+                  
+                   inquirer
+                   .prompt({
+                    name: 'next',
+                    type: "input",
+                    message: 'Would you like to place another order (Yes/No)?',
+                    })
+                  .then(function(answer) {
+                      if (answer.next === "Yes") {
+                          allProducts();
+                      } else {
+                        connection.end()
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        console.log("Thank you for shopping with us! Come back soon!")
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                      }
+                    
+                  });
+                   
+                    
+                      }
+                })
+                    }
 
-    ])
-    
-    .then (function(answer){
-        CheckUnits(answer.selectItemID, answer.selectNumUnits);
-    });     
-};
+                
+            })
+         
+        })
 
-// Once the customer has placed the order, application should check if your store has enough 
-// of the product to meet the customer's request.
-// If not, the app should log a phrase like 'Insufficient quantity!' 
-//and then prevent the order from going through.
-
-// code to update number of units in db
-function updateUnits(itemID, NumUnits, stock_quantity){
-    console.log(" Updating units".cyan);
-    var query = connection.query(
-        `UPDATE products SET stock_quantity = ${stock_quantity-NumUnits} WHERE ${itemID} = item_id`, function(err, results){
-            if (err){
-                throw err;
-            }
-            console.log("Your order has been placed successfully!".bold);
+        
         }
-    )
-}
-
-function CheckUnits(ItemID, NumUnits){
-
-
-  // query the database for the unit count of items being sold
-  connection.query(`SELECT stock_quantity FROM products WHERE ${ItemID} = products.item_id`, function(err, results) {
-    //   console.log(results);
-    //   console.log(results[0].stock_quantity);
-    //   console.log(NumUnits);
-    if (err){
-        throw err;
-    }
-    else if (parseInt(NumUnits)> results[0].stock_quantity) {
-
-        console.log ("Insufficient quantity - cannot complete your order.")
-        start();
-        }
-
-// if your store does have enough of the product, you should fulfill the customer's order.
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
-    else if (parseInt(NumUnits) <= results[0].stock_quantity) {
-
-    // once you have the items, prompt the user for which they'd like to bid on
-    updateUnits(ItemID, NumUnits, results[0].stock_quantity);
-    connection.end();
-    };
-    })
-}
