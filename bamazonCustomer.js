4d0b5aa4b6d5b5bdc8c03e26f92c1f9c5129366d
Requ Amer <requamer@Requs-MacBook-Pro.local>
2018-12-16 11:45:28 -0800

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,95 +1,79 @@
 var mysql = require('mysql');
-var inquirer = require('inquirer');
+var prompt = require('prompt');
 
+// object variable containing settings for connecting to the MySQL DB
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root", //Your username
-  password: "", //Your password
-  database: "bamazon"
+  host : 'localhost',
+  user : 'root',
+  password : 'bootcamp',
+  database : 'bamazon'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  start();
-});
-
-var max, col = ['Item ID', 'Product Name', 'Price'];
-
-var start = function() {
-  // Select all rows of columns desired and print
-  var query = Bamazon.createQuery(col);
-  connection.query(query, function(err, res) {
-    handleQuery(res);
-  });
-};
-
-var handleQuery = function(res) {
-  Bamazon.printData(res,col);
-  max = res[res.length - 1]['Item ID'];
-  chooseItem(max);
-};
-
-var chooseItem = function(max) {
-  inquirer.prompt([{
-    name: "id",
-    type: "input",
-    message: "What is the item ID of the product you would like to buy?",
-    validate: function(value) {
-      if (value>=0 && value<=max && value%1 === 0 && value.indexOf(' ')<0 && value.indexOf('.')<0) {
-        return true;
-      } else {
-        return 'Please type a whole number between 1 and ' + max + ' without a period or extra spaces';
-      }
+// prompt setup
+var schema = {
+  properties: {
+    ItemID: {
+      message: 'Enter the Item ID of the product you wish to purchase',
+      required: true
+    },
+    quantity: {
+      message: 'Enter the quantity you wish to purchase',
+      required: true
     }
-  } , {
-    name: "quantity",
-    type: "input",
-    message: "How many would you like to buy?",
-    validate: Bamazon.validateQuantity
-  }]).then(function(answer) {
-    checkQuantity(answer);
+  }
+};
+
+// shows inventory from DB then runs the prompt function which starts a cascade of nested function calls
+function showInventory() {
+  connection.query('SELECT ItemID, ProductName, Price FROM products', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Available products:');
+    for(var i = 0; i < rows.length; i++) {
+      console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price);
+    }
+    runPrompt();
   });
 };
 
-var checkQuantity = function(answer) {
-  var query = 'SELECT StockQuantity, Price, DepartmentName FROM Products WHERE ItemID = ?';
-  var params = answer.id;
-  connection.query(query, params, function(err, res) {
-    if (res[0].StockQuantity < answer.quantity) {
-      console.log(chalk.bold.red('Insufficient quantity.  Please select a quantity equal to or below ' + res[0].StockQuantity) + '.');
-      chooseItem(max);
+function runPrompt() {
+  prompt.start();
+  prompt.get(schema, function(err, result) {
+    var orderedProductID = result.ItemID;
+    var orderQuantity = result.quantity;
+    processOrder(orderedProductID, orderQuantity);
+  });
+}
+
+function processOrder(id, quantity) {
+  connection.query('SELECT StockQuantity FROM products WHERE ItemID = ?', [id], function(err, rows, fields) {
+    if(err) throw err;
+
+    if(JSON.parse(rows[0].StockQuantity) >= quantity) {
+      var adjQuantity = rows[0].StockQuantity - quantity;
+      getPrice(id, quantity);
+      updateStock(adjQuantity, id);
     } else {
-      var total = answer.quantity * res[0].Price;
-      var newQuantity = res[0].StockQuantity-answer.quantity;
-      updateQuantity(answer.id,total,newQuantity);
-      queryTotal(res[0].DepartmentName,total);
+      console.log('There is not enough stock to fulfill your request, please try again');
+      connection.end();
     }
   });
-};
+}
 
-var updateQuantity = function(id,total,newQuantity) {
-  var query = 'UPDATE Products SET StockQuantity = ? WHERE ItemID = ?';
-  var params = [newQuantity,id];
-  connection.query(query, params, function(err, res) {
-    console.log(chalk.bold.blue('\nTotal cost: ') + chalk.bold.yellow(accounting.formatMoney(total)));
-    console.log(chalk.bold.blue('Thank you come again!'));
+function getPrice(id, quantity) {
+  connection.query('SELECT Price FROM products WHERE ItemID = ?', [id], function(err, rows, fields) {
+    if(err) throw err;
+    var orderPrice = JSON.parse(rows[0].Price) * quantity;
+    console.log('The total order cost is: $' + orderPrice);
   });
-};
+}
 
-var queryTotal = function(deptName,total) {
-  var query = 'SELECT ProductSales FROM Departments WHERE DepartmentName = ?';
-  var params = deptName;
-  connection.query(query, params, function(err, res) {
-    updateTotal(res,deptName,total);
-  });
-};
-
-var updateTotal = function(res,deptName,total) {
-  var prodSales = res[0].ProductSales + total;
-  var query = 'UPDATE Departments SET ProductSales = ? WHERE DepartmentName = ?';
-  var params = [prodSales,deptName];
-  connection.query(query, params, function(err, res) {
+function updateStock(adjQuantity, id) {
+  connection.query('UPDATE products SET stockQuantity = ? WHERE ItemID = ?', [adjQuantity, id], function(err, rows, fields) {
+    if(err) throw err;
+    console.log('Inventory has been updated');
     connection.end();
   });
-};
+}
+
+// runs the program by calling the first function which then waterfalls through nested function calls
+showInventory();
